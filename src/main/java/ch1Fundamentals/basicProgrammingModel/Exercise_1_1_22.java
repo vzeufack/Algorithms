294261463704c526a1b247c807a90eee4e3a30a9
Vannel Zeufack <zvanel@live.fr>
2023-06-19 21:00:02 -0400

    Exercises 1_1_22 & 1_1_23

--- src/main/java/ch1Fundamentals/basicProgrammingModel/Exercise_1_1_22.java
+++ src/main/java/ch1Fundamentals/basicProgrammingModel/Exercise_1_1_22.java
@@ -1,25 +1,33 @@
 package ch1Fundamentals.basicProgrammingModel;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.security.SecureRandom;
 import java.util.Arrays;
 
 public class Exercise_1_1_22 {
     public static void main(String[] args) {
-        SecureRandom random = new SecureRandom();
-        int[] array = random.ints(20, 0, 100).toArray();
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(indexOf(array, 16));
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+        Arrays.sort(whitelist);
+
+        if(args[1].equals("+")) {
+            while (!StdIn.isEmpty()) {
+                int key = StdIn.readInt();
+                if (indexOf(whitelist, key) == -1)
+                    StdOut.println(key);
+            }
+        }
+        else{
+            StdOut.println(Arrays.toString(whitelist));
+        }
     }
     public static int indexOf(int[] a, int key){
         return indexOf(a, key, 0, a.length - 1, 1);
     }
 
     public static int indexOf(int[] a, int key, int lo, int hi, int depth){
-        StdOut.printf("%" + depth + "s %d %d\n", "", lo, hi);
-
         if(lo > hi)
             return -1;
 
